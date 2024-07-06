4e94442f73399561c1d44faabe1b112e16ca0a61
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-07-07 02:55:30 +0600

    install react-bootstrap, bootstrap, react-router-dom,react-toastify, axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import {Route, Routes} from 'react-router-dom';
+
 const App = () => (
     <>
+      <Routes>
+        <Route path="/" element={<div/>}/>
 
+        <Route path="*" element={<h1>Not found!</h1>} />
+      </Routes>
     </>
 );
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {BrowserRouter} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ToastContainer position={"bottom-left"} />
+      <App />
+    </BrowserRouter>,
   </React.StrictMode>,
 )
